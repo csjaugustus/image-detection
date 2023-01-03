994a7d10c9a17ec0709a57ea2bd99e19bd436678
Augustus Chen <csjaugustus@hotmail.com>
2023-01-03 21:29:55 +0800

    fixed click position so that it doesnt always click the dead center

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import cv2
 import numpy as np
 import pyautogui
-from random import choice
+from random import choice, randint
 from win32gui import GetForegroundWindow, GetWindowRect
 import time
 import os
 
 def base_detect_image(template_file_name, threshold = 0.9):
-	"""
-	A base detection algorithm that detects images through template matching.
-	Takes an image file name, a threshold value specifiying detection accuracy,
-	and returns the (x_min, x_max, y_min, y_max) of the nearest detected image.
-	Does not distinguish between colours.
-	"""
 	if not os.path.exists(template_file_name):
 		raise FileNotFoundError
 
@@ -37,11 +31,6 @@ def base_detect_image(template_file_name, threshold = 0.9):
 		return False
 
 def detect_image(template_file_name, delay=3, threshold=0.9):
-	"""
-	Takes an image file name, a delay (if a wait time may be needed for image to appear) a threshold value specifying detection accuracy,
-	and returns the center coords of the nearest detected image.
-	Returns False if image is undetected.
-	"""
 	t = 0
 	while not base_detect_image(template_file_name, threshold=threshold):
 		time.sleep(0.1)
@@ -52,14 +41,21 @@ def detect_image(template_file_name, delay=3, threshold=0.9):
 	x1, x2, y1, y2 = base_detect_image(template_file_name, threshold=threshold)
 	center_x = (x1 + x2)/2
 	center_y = (y1 + y2)/2
-	return (center_x, center_y)
+
+	#this will make sure it doesn't click on the center all the time
+	x_coord = randint(int((x2-x1)/3 + x1), int((x2-x1)/3 * 2 + x1))
+	y_coord = randint(int((y2-y1)/3 + y1), int((y2-y1)/3 * 2 + y1))
+
+	# return (center_x, center_y)
+	return (x_coord, y_coord)
+
+def crop_full(detected):
+	x_min, x_max, y_min, y_max = detected
+	ss = pyautogui.screenshot()
+	cropped = ss.crop((x_min-80, y_min-50, x_max+100, y_max+15))
+	return cropped
 
 def click_image(template_file_name, delay=3, double=False, threshold=0.9):
-	"""
-	Takes an image file name, a delay (if a wait time may be needed for image to appear) a threshold value specifying detection accuracy,
-	clicks (optionally double clicks) the center of detected image, and returns the center coords of the nearest detected image.
-	Returns False if image is undetected.
-	"""
 	try:
 		center_x, center_y = detect_image(template_file_name, delay=delay, threshold=threshold)
 	except TypeError:
@@ -73,11 +69,6 @@ def click_image(template_file_name, delay=3, double=False, threshold=0.9):
 	return (center_x, center_y)
 
 def move_to_image(template_file_name, delay=3, threshold=0.9):
-	"""
-	Takes an image file name, a delay (if a wait time may be needed for image to appear) a threshold value specifying detection accuracy,
-	moves cursor to the center of detected image, and returns the center coords of the nearest detected image.
-	Returns False if image is undetected.
-	"""
 	try:
 		center_x, center_y = detect_image(template_file_name, delay=delay, threshold=threshold)
 	except TypeError:
@@ -86,18 +77,10 @@ def move_to_image(template_file_name, delay=3, threshold=0.9):
 
 	return (center_x, center_y)
 
-def crop_full(detected, left, right, top, bottom):
-	"""
-	Takes a detected image (returned result of a detection), and returns a larger screenshot which includes image surrounding detected image.
-	The left right top bottom values specify how large the surrounding image should be.
-	"""
-	x_min, x_max, y_min, y_max = detected
-	ss = pyautogui.screenshot()
-	cropped = ss.crop((x_min-80, y_min-50, x_max+100, y_max+15))
-	return cropped
-
 def main():
-	move_to_image("example.png")
+	# result = click_image("files\\1.png", delay=5)
+	# # crop_full(result).save("cropped.png")
+	move_to_image("automation\\wjcszs1.png")
 
 if __name__ == "__main__":
 	main()
